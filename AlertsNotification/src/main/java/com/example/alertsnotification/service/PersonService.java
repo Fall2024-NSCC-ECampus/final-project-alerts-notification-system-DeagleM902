c1c85a55001f4e39be9c6293c4eb69023378c7ec
DeagleM <W0258876@campus.nscc.ca>
2024-12-06 13:20:14 -0400

    updated comments

--- AlertsNotification/src/main/java/com/example/alertsnotification/service/PersonService.java
+++ AlertsNotification/src/main/java/com/example/alertsnotification/service/PersonService.java
@@ -18,14 +18,27 @@ import java.util.stream.Collectors;
 public class PersonService {
     private final PersonRepository personRepository;
 
+    /**
+     * Constructs a PersonRepository with the specified repository
+     * @param personRepository The repository of residents.
+     */
     public PersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
 
+    /**
+     * Retrieves a list of all residents/Persons.
+     * @return A list of all Person objects.
+     */
     public List<Person> getAllPersons() {
         return personRepository.findAll();
     }
 
+    /**
+     * Retrieves the residents at a specified address.
+     * @param address The address to be searched.
+     * @return The information of the residents at the given address.
+     */
     public List<Person> getPersonsByAddress(String address) {
         List<Person> residents = personRepository.findByAddress(address);
         if (residents.isEmpty()) {
@@ -34,10 +47,21 @@ public class PersonService {
         return residents;
     }
 
+    /**
+     * Retrieves the information of all residents with the given name.
+     * @param firstName The first name to search.
+     * @param lastName The last name to search.
+     * @return The information of all residents with the given first / last name combination.
+     */
     public List<Person> getPersonsByName(String firstName, String lastName) {
         return personRepository.findByFirstNameAndLastName(firstName, lastName);
     }
 
+    /**
+     * Retrieves the name and email of all residents of a given city.
+     * @param city The city to search.
+     * @return The name and email of all residents of the given city.
+     */
     public List<CommunityEmailDTO> getEmailByCity(String city) {
         return personRepository.findEmailByCity(city);
     }
